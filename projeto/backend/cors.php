5b6037606b05caa35899127377fce4d3ed937584
joao victor <jvsampaiio15@gmail.com>
2026-09-04 12:19:49 -0300

    Corrige tratamento do Preflight OPTIONS via PHP

--- projeto/backend/cors.php
+++ projeto/backend/cors.php
@@ -6,7 +6,6 @@ header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header("Content-Type: application/json; charset=UTF-8");
 
-// Trata requisições de verificação (Preflight OPTIONS)
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
